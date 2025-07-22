0d63b93b12fc390c4fc68e01475cf2635aed7f81
AdityaGaikwad03 <gaikwadaditya0309@gmail.com>
2025-07-22 12:46:53 +0530

    solved

---
+++ MERN_Fundation/Problem_178.js
@@ -0,0 +1,81 @@
+/* Sort JSON Object - Descending
+You are given a JSON Array of the format below
+
+[ 
+  {
+    "title": "sampleTitle1",
+    "author": "sampleAuthor1",
+    "libraryID": 345
+  },
+  {
+    "title": "sampleTitle2",
+    "author": "sampleAuthor2",
+    "libraryID": 123
+  },
+  {
+    "title": "sampleTitle3",
+    "author": "sampleAuthor3",
+    "libraryID": 567
+  }
+]
+Your task is to complete the function DescendingSort() which sorts this array on the basis of the key libraryID in Descending order and returns the sorted array.
+
+Input Format
+First line contains the JSON array of objects, in compact form(in a single line)
+
+Output Format
+If there are n objects, then there are n line printed in output in the format sampleTitle1-sampleAuthor1-345
+
+Example 1
+Input
+
+[{"title":"Bill Gates","author":"The Road Ahead","libraryID":1254},{"title":"Steve Jobs","author":"Walter Isaacson","libraryID":4264},{"title":"Mockingjay: The Final Book of The Hunger Games","author":"Suzanne Collins","libraryID":3245}]
+Output
+
+Steve Jobs-Walter Isaacson-4264
+Mockingjay: The Final Book of The Hunger Games-Suzanne Collins-3245
+Bill Gates-The Road Ahead-1254
+Explanation
+
+The JSON object for this testcase is
+
+[
+  {
+    "title": "Bill Gates",
+    "author": "The Road Ahead",
+    "libraryID": 1254
+  },
+  {
+    "title": "Steve Jobs",
+    "author": "Walter Isaacson",
+    "libraryID": 4264
+  },
+  {
+    "title": "Mockingjay: The Final Book of The Hunger Games",
+    "author": "Suzanne Collins",
+    "libraryID": 3245
+  }
+]
+So we can after sorting the objects on the basis of libraryID, we get the output in the format given above.
+
+Constraints
+1 <= Size of Input Array <= 1000 */
+
+function DecendingSort(a, b) {
+    //Write your code here
+  return b.libraryID - a.libraryID;
+}
+
+
+// DO NOT CHANGE THE CODE BELOW
+var readline = require("readline").createInterface(process.stdin);
+readline.on("line", readInputs);
+
+function readInputs(line) {
+  let obj = JSON.parse(line);
+  var sortedObj = obj.sort(DecendingSort)
+  for(var i = 0; i < sortedObj.length; i++) {
+    console.log(sortedObj[i].title + '-' + sortedObj[i].author + '-' + sortedObj[i].libraryID);
+  }
+  readline.close();
+}
